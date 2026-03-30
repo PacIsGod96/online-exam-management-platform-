7471397e338d74035902ee226b7c8cc7928f0b9a
Joshua Cuebas <joshcuebas333@aol.com>
2026-03-30 07:44:10 -0400

    template and show all test page
    git commit -m template

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-sqlalchemy
+@app.route('/test_create')
+def test_create():
+    return render_template("test_create.html")
+
+@app.route('/test_page',method =["GET"])
+def test_page():
+    result = conn.execute(text("SELECT * FROM tests ORDER BY created_at DESC")).fetchall()
+    return render_template("test_page.html",tests = result)
+
+@app.route('/test_taken')
+def test_taken():

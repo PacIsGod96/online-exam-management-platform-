b32fc8a93b9e8bdac660a5c8e1f7e04f67cce9a2
Collin <williamsck233@gmail.com>
2026-04-03 02:07:20 -0400

    worked on trying to finsih the project just need o fix a few bugs

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,21 +1,24 @@
-@app.route('/all_acount/<int:user_id>')
-def all_acount(user_id):
-    role = request.args.get("role")
+@app.route('/test_create/<int:user_id>',methods=["GET", "POST"])
+def test_create(user_id):
     user = conn.execute(
-        text("SELECT account_id, role FROM accounts WHERE account_id = :id"),
-        {"id": user_id}
+    text("SELECT account_id, role, first_name, last_name FROM accounts WHERE account_id = :id"),
+    {"id": user_id}
     ).mappings().fetchone()
-    if role:
-        sql = text("""
-            SELECT *
-            FROM accounts
-            WHERE role = :role
-            ORDER BY account_id DESC
-        """)
-        result = conn.execute(sql, {"role": role}).fetchall()
-    else:
-        result = conn.execute(
-            text("SELECT * FROM accounts ORDER BY account_id DESC")
-        ).fetchall()
+    if request.method == "POST":
+        title = request.form["title"]
+        test_disc = request.form ["test_disc"]
+        created_by = request.form ["created_by"]
+        teacher_name = user['first_name'] + " " + user['last_name']
 
-    return render_template("all_acount.html", accounts=result,user=user)
+        conn.execute(
+            text(
+                "INSERT INTO tests (title, test_disc, created_by) "
+                "VALUES (:title, :test_disc, :created_by)"
+            ),
+            {"title": title, "test_disc": test_disc, "created_by": teacher_name}
+        )
+
+        conn.commit()
+
+
+    return render_template("test_create.html",user=user)
